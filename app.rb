64af1e2cc1a28835da328cac2a251f415346dedd
Caleb Francis <calebonesimus@gmail.com>
2015-07-20 14:18:34 -0400

    Add author info dynamically

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
 get '/' do
+  @author_info = {name: "Anya",
+                  date_published: "July 12, 2015"}
   erb :index
+end
+
+get '/second-article' do
+  @author_info = {name: "Caleb",
+                  date_published: "July 20, 2015"}
+  erb :second_article
 end
