3abbb6e20527d1164d72f941ca19b1e8eda8b492
Caleb Francis <calebonesimus@gmail.com>
2015-07-20 15:12:23 -0400

    Make second article work with/without params

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 
+# Goes to main article
 get '/' do
   @author_info = {name: "Anya",
                   date_published: "July 12, 2015",
@@ -7,6 +8,16 @@ get '/' do
   erb :index
 end
 
+# Goes to a similar article with different author data
+get '/second-article' do
+  @author_info = {name: "Caleb",
+                  date_published: "July 20, 2015",
+                  photo: "/images/beer.jpg"}
+  @headline = "Change me with the URL!"
+  erb :second_article
+end
+
+# Second article with sidebar headline parameter, for fun.
 get '/second-article/:headline' do |headline|
   @author_info = {name: "Caleb",
                   date_published: "July 20, 2015",
