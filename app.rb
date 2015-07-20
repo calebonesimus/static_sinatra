7088999a05235f7754363d4b4856515e655f8ae0
Caleb Francis <calebonesimus@gmail.com>
2015-07-20 15:06:06 -0400

    Add dynamic data

--- app.rb
+++ app.rb
@@ -2,12 +2,15 @@ require 'sinatra'
 
 get '/' do
   @author_info = {name: "Anya",
-                  date_published: "July 12, 2015"}
+                  date_published: "July 12, 2015",
+                  photo: "/images/anya.jpg"}
   erb :index
 end
 
-get '/second-article' do
+get '/second-article/:headline' do |headline|
   @author_info = {name: "Caleb",
-                  date_published: "July 20, 2015"}
+                  date_published: "July 20, 2015",
+                  photo: "/images/beer.jpg"}
+  @headline = headline
   erb :second_article
 end
